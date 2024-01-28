80fd120e5b9a5a52aadf7ec9dd9df89e85c70413
seymurjafarov <jafar.seymur@gmail.com>
2024-01-28 23:04:45 +0400

    added MathUtils class

---
+++ src/utils/MathUtils.java
@@ -0,0 +1,19 @@
+package utils;
+
+public class MathUtils {
+    public static double plus(double a, double b) {
+        return a + b;
+    }
+
+    public static double minus(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiple(double a, double b) {
+        return a * b;
+    }
+
+    public static double divide(double a, double b) {
+        return a / b;
+    }
+}
